a6310ec6831ecb9d8327933853c3b154b34371e8
MellyMel Melynk <103768697+orthmanh-snowflake@users.noreply.github.com>
2022-04-21 15:51:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,11 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 #take the json version of the resonse and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+##fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+##streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
