d607256ef6d8091a8ff8296ee5b3d788573adfb3
MellyMel Melynk <103768697+orthmanh-snowflake@users.noreply.github.com>
2022-04-26 10:05:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Apple')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'banana')
 streamlit.write('The user entered', fruit_choice)
 
 import requests
@@ -34,11 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit laod list contains:")
-streamlit.dataframe(my_data_rows)
